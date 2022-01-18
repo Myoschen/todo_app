ad57ad1e2a90d4df53bf180bf27e4b4e683b1d16
willy14620 <willy14620@gmail.com>
2022-01-18 14:52:28 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <div className='mt-8'>
-        <h1 className='text-6xl text-center mb-16 font-bold hidden sm:block'>Todo App</h1>
+        <h1 className='text-6xl text-center mb-16 font-bold hidden sm:block'>Todo</h1>
         <div className='mb-8 flex sm:justify-around gap-2'>
           <input onChange={e => handleChange(e.target.value)} value={input} className='p-1 px-2 focus:outline-1 outline-slate-300 text-lg' type='text' placeholder='Todo something ...' />
           <button onClick={handleAddTodo} aria-label='submit' className='bg-slate-300 py-2 px-4 rounded hover:bg-slate-200 active:bg-slate-400 shadow transition-colors duration-300 text-lime-600'><ImPlus /></button>
